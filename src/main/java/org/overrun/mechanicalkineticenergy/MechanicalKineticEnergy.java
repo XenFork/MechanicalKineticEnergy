f96694a95109c30274c4f76b551ebb971f292cbf
baka4n <qq474899581@gmail.com>
2022-12-23 02:36:40 +0800

    更新

--- src/main/java/org/overrun/mechanicalkineticenergy/MechanicalKineticEnergy.java
+++ src/main/java/org/overrun/mechanicalkineticenergy/MechanicalKineticEnergy.java
@@ -1,18 +1,36 @@
 package org.overrun.mechanicalkineticenergy;
 
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Server;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MechanicalKineticEnergy extends JavaPlugin {
+import java.util.logging.Logger;
 
+public final class MechanicalKineticEnergy extends JavaPlugin {
+    private final Logger log = getLogger();
+    private final Server server = getServer();
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("load plugin...");
+        log.info("load plugin...");
+        ItemStack stack = new ItemStack(Material.IRON_INGOT);
+        NamespacedKey ashjKey = new NamespacedKey(this, "andesite_alloy");
+        ShapedRecipe ashj = new ShapedRecipe(ashjKey,stack);
+        ashj.shape(
+                "xy",
+                "yx"
+        )
+                .setIngredient('x', Material.ANDESITE)
+                .setIngredient('y', Material.IRON_NUGGET);
+        server.addRecipe(ashj);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("disable plugin...");
+        log.warning("disable plugin");
     }
 }
