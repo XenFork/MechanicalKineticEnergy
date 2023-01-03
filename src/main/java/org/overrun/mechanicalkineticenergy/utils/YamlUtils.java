93f3999116683a73d7f84617addfc74820d37159
baka4n <qq474899581@gmail.com>
2023-01-03 18:37:33 +0800

    更新 YamlUtils.java

--- src/main/java/org/overrun/mechanicalkineticenergy/utils/YamlUtils.java
+++ src/main/java/org/overrun/mechanicalkineticenergy/utils/YamlUtils.java
@@ -13,124 +13,95 @@ public class YamlUtils {
     public static final List<Record> itemRecord = new ArrayList<>();
     public static final List<RecordShaped> shapedRecord = new ArrayList<>();
 
+    public static MemorySection getMemorySection(MemorySection memorySection, String key) {
+        Object o = memorySection.get(key);
+        if (o instanceof MemorySection m1) {
+            return m1;
+        }
+        return null;
+    }
+    public static List<String> getMKeyList(MemorySection memorySection) {
+        return memorySection.getKeys(true).stream().filter(s -> !s.contains(".")).toList();
+    }
+
     public static void invokeShaped(FileConfiguration mkeConfig) {
-        Object shaped = mkeConfig.get("Shaped");
-        if (shaped instanceof MemorySection m) {
-            for (String key : m.getKeys(true).stream().filter(s -> !s.contains(".")).toList()) {
-                RecordShaped recordShaped = new RecordShaped();
-                recordShaped.recipeName = key;
-                Object o = m.get(key);
-                if (o instanceof MemorySection m1) {
-                    for (String m1Key : m1.getKeys(true).stream().filter(s -> !s.contains(".")).toList()) {
-
-                        switch (m1Key) {
-                            case "out" -> {
-                                Object o1 = m1.get(m1Key);
-                                if (o1 instanceof MemorySection m2) {
-                                    for (String m2Key : m2.getKeys(true).stream().filter(s -> !s.contains(".")).toList()) {
-                                        recordShaped.outNameSpaceKey = m2Key;
-                                        recordShaped.outSubName = m2.getString(m2Key);
-                                    }
-                                }
-                            }
-                            case "shaped" -> recordShaped.shape.addAll(m1.getStringList(m1Key));
-                            case "ingredient" -> {
-
-                                Object o1 = m1.get(m1Key);
-                                if (o1 instanceof MemorySection m2) {
-                                    for (String m2Key : m2.getKeys(true).stream().filter(s -> !s.contains(".")).toList()) {
-                                        System.out.println(m2Key);
-                                        Object o2 = m2.get(m2Key);
-                                        RecordNameSpace nameSpace = new RecordNameSpace();
-                                        nameSpace.key = m2Key;
-                                        if (o2 instanceof MemorySection m3) {
-                                            for (String m3Key : m3.getKeys(true).stream().filter(s -> !s.contains(".")).toList()) {
-                                                nameSpace.value = m3.getString(m3Key);
-                                                recordShaped.ingredientMap.put(m3Key.charAt(0), nameSpace);
-                                            }
-                                        }
-                                    }
-                                }
-
-                            }
-                            case "mirror" -> recordShaped.mirror = m1.getBoolean("mirror");
+        MemorySection m = getMemorySection(mkeConfig, "Shaped");
+        if (m != null) for (String key : getMKeyList(m)) {
+            RecordShaped recordShaped = new RecordShaped();
+            recordShaped.recipeName = key;
+            MemorySection m1 = getMemorySection(m, key);
+            if (m1 != null) for (String m1Key : getMKeyList(m1)) {
+                switch (m1Key) {
+                    case "out" -> {
+                        MemorySection m2 = getMemorySection(m1, m1Key);
+                        if (m2 != null) for (String m2Key : getMKeyList(m2)) {
+                            recordShaped.outNameSpaceKey = m2Key;
+                            recordShaped.outSubName = m2.getString(m2Key);
                         }
                     }
+                    case "shaped" -> recordShaped.shape.addAll(m1.getStringList(m1Key));
+                    case "ingredient" -> {
+                        MemorySection m2 = getMemorySection(m1, m1Key);
+                        if (m2 != null) for (String m2Key : getMKeyList(m2)) {
+                            System.out.println(m2Key);
+                            Object o2 = m2.get(m2Key);
+                            RecordNameSpace nameSpace = new RecordNameSpace();
+                            nameSpace.key = m2Key;
+                            if (o2 instanceof MemorySection m3) for (String m3Key : getMKeyList(m3)) {
+                                nameSpace.value = m3.getString(m3Key);
+                                recordShaped.ingredientMap.put(m3Key.charAt(0), nameSpace);
+                            }
+                        }
+                    }
+                    case "mirror" -> recordShaped.mirror = m1.getBoolean("mirror");
                 }
-                shapedRecord.add(recordShaped);
             }
+            shapedRecord.add(recordShaped);
         }
     }
 
     public static void invokeItem(FileConfiguration mkeConfig) {
-        Object item = mkeConfig.get("Item");
-        if (item instanceof MemorySection m) {
-            for (String key : m.getKeys(true).stream().filter(s -> !s.contains(".")).toList()) {
-                Record record = new Record();
-                record.name = key;
-                Object regItemName = m.get(key);
-                if ( regItemName instanceof MemorySection m1) {
-                    for (String m1Key : m1.getKeys(true).stream().filter(s -> !s.contains(".")).toList()) {
-                        Object o = m1.get(m1Key);
-                        switch (m1Key) {
-                            case "name" -> {
-                                if (o instanceof MemorySection m2) {
-                                    for (String m2Key : m2.getKeys(true)) {
-                                        record.namespaceKey = m2Key;
-                                        record.materialName = m2.getString(m2Key);
-                                    }
+        MemorySection m = getMemorySection(mkeConfig, "Item");
+        if (m != null) for (String key : getMKeyList(m)) {
+            Record record = new Record();
+            record.name = key;
+            MemorySection m1 = getMemorySection(m, key);
+            if (m1 != null) for (String m1Key : getMKeyList(m1)) {
+                MemorySection m2 = getMemorySection(m1, m1Key);
+                switch (m1Key) {
+                    case "name" -> {
+                        if (m2 != null) for (String m2Key : m2.getKeys(true)) {
+                            record.namespaceKey = m2Key;
+                            record.materialName = m2.getString(m2Key);
+                        }
+                    }
+                    case "nbt" -> {
+                        if (m2 != null) for (String m2Key : getMKeyList(m2)) {
+                            MemorySection m3 = getMemorySection(m2, m2Key);
+                            switch (m2Key) {
+                                case "string" -> {
+                                    if (m3 != null) for (String m3Key : m3.getKeys(true))
+                                        record.nbt.put(m3Key, m3.getString(m3Key));
                                 }
-                            }
-                            case "nbt" -> {
-                                if (o instanceof MemorySection m2) {
-                                    for (String m2Key : m2.getKeys(true).stream().filter(s -> !s.contains(".")).toList()) {
-                                        switch (m2Key) {
-                                            case "string" -> {
-                                                Object o1 = m2.get(m2Key);
-                                                if (o1 instanceof MemorySection m3) {
-                                                    for (String m3Key : m3.getKeys(true)) {
-                                                        record.nbt.put(m3Key, m3.getString(m3Key));
-                                                    }
-                                                }
-                                            }
-                                            case "int" -> {
-                                                Object o1 = m2.get(m2Key);
-                                                if (o1 instanceof MemorySection m3) {
-                                                    for (String m3Key : m3.getKeys(true)) {
-                                                        record.nbt.put(m3Key, m3.getInt(m3Key));
-                                                    }
-                                                }
-                                            }
-                                            case "double" -> {
-                                                Object o1 = m2.get(m2Key);
-                                                if (o1 instanceof MemorySection m3) {
-                                                    for (String m3Key : m3.getKeys(true)) {
-                                                        record.nbt.put(m3Key, m3.getDouble(m3Key));
-                                                    }
-                                                }
-                                            }
-                                            case "long" -> {
-                                                Object o1 = m2.get(m2Key);
-                                                if (o1 instanceof MemorySection m3) {
-                                                    for (String m3Key : m3.getKeys(true)) {
-                                                        record.nbt.put(m3Key, m3.getLong(m3Key));
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
+                                case "int" -> {
+                                    if (m3 != null) for (String m3Key : m3.getKeys(true))
+                                        record.nbt.put(m3Key, m3.getInt(m3Key));
                                 }
-                            }
-                            case "count" -> {
-                                if (o instanceof Integer i) {
-                                    record.count = i;
+                                case "double" -> {
+                                    if (m3 != null) for (String m3Key : m3.getKeys(true))
+                                        record.nbt.put(m3Key, m3.getDouble(m3Key));
+                                }
+                                case "long" -> {
+                                    if (m3 != null) for (String m3Key : m3.getKeys(true))
+                                        record.nbt.put(m3Key, m3.getLong(m3Key));
                                 }
                             }
                         }
                     }
+                    case "count" -> record.count = m1.getInt(m1Key);
                 }
-                itemRecord.add(record);
             }
+            itemRecord.add(record);
         }
 
     }
@@ -140,7 +111,7 @@ public class YamlUtils {
         private boolean mirror;
         private String outNameSpaceKey;
         private String outSubName;
-        private List<String> shape = new ArrayList<>();
+        private final List<String> shape = new ArrayList<>();
         private final Map<Character, RecordNameSpace> ingredientMap = new HashMap<>();
         public RecordShaped() {
 
